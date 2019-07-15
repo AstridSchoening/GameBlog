791829786d2e42388ba806aff794972035e76ec7
Dein Name <du@example.com>
2019-07-15 12:14:38 +0200

    puyan last

--- events/forms.py
+++ events/forms.py
@@ -5,5 +5,5 @@ class EventForm(forms.ModelForm):
 
     class Meta:
          model = Event
-         fields = ('zeitpunkt', 'title',  'author','text')
+         fields = ('zeitpunkt', 'title',  'author','text','twitchlink')
          widgets = { 'zeitpunkt': forms.DateTimeInput(attrs={'class': 'datetime-input'}) }
--- events/models.py
+++ events/models.py
@@ -8,7 +8,7 @@ class Event(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     zeitpunkt = models.DateTimeField()
-    
+    twitchlink = models.CharField(max_length=200)
 
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
